1d86e117f160e7ef44675a8aac7e2b3b2257f732
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-16 15:26:31 +0300

    Else

--- 3)ControlFlowInRuby.rb
+++ 3)ControlFlowInRuby.rb
@@ -2,3 +2,10 @@ puts "### if ###"
 if 3 < 9
   puts "I'm getting printed because three is less than nine!"
 end
+
+puts "### else ###"
+if 3 > 9
+  puts "I won't get printed because three is less than nine."
+else
+  puts "That means I'll get printed!"
+end
