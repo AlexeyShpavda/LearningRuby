d4f46865a6f2a883733fdbba06362c6c951d3afc
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-16 15:55:04 +0300

    Or

--- 3)ControlFlowInRuby.rb
+++ 3)ControlFlowInRuby.rb
@@ -46,3 +46,9 @@ puts true && true # => true
 puts true && false # => false
 puts false && true # => false
 puts false && false # => false
+
+puts "### Or ###"
+puts true || true # => true
+puts true || false # => true
+puts false || true # => true
+puts false || false # => false
