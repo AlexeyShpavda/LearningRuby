fc5dae39dad0a51a72c30cd5109cf34deba6ec0c
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-16 15:08:29 +0300

    Formatting with String Methods

--- 2)PuttingTheFormInFormatter.rb
+++ 2)PuttingTheFormInFormatter.rb
@@ -6,3 +6,8 @@ name = gets.chomp
 
 puts "### Printing the Output ###"
 puts "Your name is #{name}!"
+
+puts "### Formatting with String Methods ###"
+puts "What's your last name?"
+surname = gets.chomp
+puts "Your name is #{surname.capitalize}!"
