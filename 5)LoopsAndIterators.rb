0d045840074caeda52a88fb33176d490de815050
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-17 18:18:37 +0300

    The 'Until' Loop

--- 5)LoopsAndIterators.rb
+++ 5)LoopsAndIterators.rb
@@ -4,3 +4,11 @@ while counter < 11
   print counter, " "
   counter = counter + 1
 end
+puts
+
+puts "### The 'Until' Loop ###"
+counter = 1
+until counter > 10
+  print counter, " "
+  counter = counter + 1
+end
