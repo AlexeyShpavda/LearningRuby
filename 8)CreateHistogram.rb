5a58a63451eda19ab8915b3354be795d55f3df1a
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-19 01:23:57 +0300

    Iterating Over the Array

--- 8)CreateHistogram.rb
+++ 8)CreateHistogram.rb
@@ -7,3 +7,8 @@ words = text.split(" ")
 
 puts "### Creating the Frequencies Hash ###"
 frequencies = Hash.new(0)
+
+puts "### Iterating Over the Array ###"
+words.each do |word|
+  frequencies[word]+=1
+end
