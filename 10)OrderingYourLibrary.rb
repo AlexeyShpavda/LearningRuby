8246e23e71d67b43feff31f5c2f8b221dd80ae73
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-21 00:27:09 +0300

    Sorting With Control Flow

--- 10)OrderingYourLibrary.rb
+++ 10)OrderingYourLibrary.rb
@@ -7,10 +7,25 @@ puts "### Default Parameters ###"
 # end
 
 puts "### Sorting ###"
-def alphabetize(arr, rev=false)
-  arr.sort!
-end
-
+=begin
+  def alphabetize(arr, rev=false)
+    arr.sort!
+  end
 numbers = [5, 1, 3, 8]
 alphabetize(numbers)
 puts numbers
+=end
+
+puts "### Sorting With Control Flow ###"
+def alphabetize(arr, rev=false)
+  if rev
+    arr.sort!.reverse!
+  else
+    arr.sort!
+  end
+end
+
+numbers = [5, 1, 3, 8]
+print numbers
+alphabetize(numbers)
+print numbers
