157851462b8968bc16f866eaf406296c99020cc3
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-21 00:21:17 +0300

    Sorting

--- 10)OrderingYourLibrary.rb
+++ 10)OrderingYourLibrary.rb
@@ -3,5 +3,14 @@ puts "### Defining Our Method ###"
 # end
 
 puts "### Default Parameters ###"
+# def alphabetize(arr, rev=false)
+# end
+
+puts "### Sorting ###"
 def alphabetize(arr, rev=false)
+  arr.sort!
 end
+
+numbers = [5, 1, 3, 8]
+alphabetize(numbers)
+puts numbers
