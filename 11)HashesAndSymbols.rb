3a15d698dbda205f77c93b1292182bb6df67afdb
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-23 19:05:42 +0300

    More Methods, More Solutions

--- 11)HashesAndSymbols.rb
+++ 11)HashesAndSymbols.rb
@@ -125,3 +125,20 @@ movie_ratings = {
 
 good_movies = movie_ratings.select { |key, value| value > 3 }
 puts good_movies
+
+puts "### More Methods, More Solutions ###"
+movie_ratings = {
+    memento: 3,
+    primer: 3.5,
+    the_matrix: 3,
+    truman_show: 4,
+    red_dawn: 1.5,
+    skyfall: 4,
+    alex_cross: 2,
+    uhf: 1,
+    lion_king: 3.5
+}
+
+movie_ratings.each_key { |k| puts k }
+
+movie_ratings.each_value { |v| puts v }
