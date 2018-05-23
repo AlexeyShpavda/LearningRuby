04763c371f7bd16c531049b330186ad0dbe365e6
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-23 18:32:56 +0300

    Iterating Over Hashes

--- 11)HashesAndSymbols.rb
+++ 11)HashesAndSymbols.rb
@@ -1,2 +1,14 @@
 puts "### The Story So Far ###"
 my_hash = Hash.new
+
+puts "### Iterating Over Hashes ###"
+matz = { "First name" => "Yukihiro",
+         "Last name" => "Matsumoto",
+         "Age" => 47,
+         "Nationality" => "Japanese",
+         "Nickname" => "Matz"
+}
+
+matz.each do |key, value|
+  puts value
+end
