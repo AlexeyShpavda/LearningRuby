b05cf120fb807dff055ff7ce14177f1b97856f87
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-23 18:39:05 +0300

    A Key of a Different Color

--- 11)HashesAndSymbols.rb
+++ 11)HashesAndSymbols.rb
@@ -26,3 +26,12 @@ creatures["birds"]
 
 puts "### Setting Own Default ###"
 no_nil_hash = Hash.new("Hello")
+
+puts "### A Key of a Different Color ###"
+menagerie = { :foxes => 2,
+              :giraffe => 1,
+              :weezards => 17,
+              :elves => 1,
+              :canaries => 4,
+              :ham => 1
+}
