fdaeed86d99fd11ca50ca7a2eb38c8baec58ccfc
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-24 23:18:12 +0300

    Update

--- 12)NightAtTheMovie.rb
+++ 12)NightAtTheMovie.rb
@@ -69,6 +69,7 @@ end
 =end
 
 puts "### Error! Error! ###"
+=begin
 case choice
   when "add"
     puts "What movie would you like to add? "
@@ -89,4 +90,36 @@ case choice
     puts "Deleted!"
   else
     puts "Error!"
+end
+=end
+
+puts "### Update ###"
+case choice
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+  puts movies
+when "update"
+  puts "What movie would you like to update? "
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Error!"
+  else
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
 end
