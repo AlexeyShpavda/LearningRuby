f7bf2bc8a98d18a4efc704acc3dc3ef42a1fb719
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-24 22:53:41 +0300

    Setting Up

---
+++ 12)NightAtTheMovie.rb
@@ -0,0 +1,13 @@
+puts "### Setting Up ###"
+movies = {
+    Interstellar: 4.8,
+    TheDarkKnight: 4.7
+}
+
+puts "What would you like to do? "
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
+choice = gets.chomp
