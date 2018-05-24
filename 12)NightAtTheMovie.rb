1f88a8905ffe0a40f79f05b71adebc3b060cbd65
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-24 23:26:53 +0300

    Delete

--- 12)NightAtTheMovie.rb
+++ 12)NightAtTheMovie.rb
@@ -127,6 +127,7 @@ end
 =end
 
 puts "### Display ###"
+=begin
 case choice
   when "add"
     puts "What movie would you like to add? "
@@ -157,4 +158,45 @@ case choice
     puts "Deleted!"
   else
     puts "Error!"
+end
+=end
+
+puts "### Delete ###"
+case choice
+  when "add"
+    puts "What movie would you like to add? "
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "What rating does the movie have? "
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+    else
+      puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+    end
+    puts movies
+  when "update"
+    puts "What movie would you like to update? "
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "Error!"
+    else
+      puts "What rating does the movie have? "
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+    end
+  when "display"
+    movies.each do |movie, rating|
+      puts "#{movie}: #{rating}"
+    end
+  when "delete"
+    puts "What movie do you want to delete?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "Movie not found!"
+    else
+      movies.delete(title.to_sym)
+      puts "#{title} has been removed."
+    end
+  else
+    puts "Sorry, I didn't understand you."
 end
