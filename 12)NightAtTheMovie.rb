14635c07115450e9fc650e9d048ae158fe2126b7
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-24 23:06:56 +0300

    Prompting: Redux!

--- 12)NightAtTheMovie.rb
+++ 12)NightAtTheMovie.rb
@@ -13,9 +13,29 @@ puts "-- Type 'delete' to delete a movie."
 choice = gets.chomp
 
 puts "### The Case Statement ###"
+=begin
 case choice
   when "add"
     puts "Added!"
+  when "update"
+    puts "Updated!"
+  when "display"
+    puts "Movies!"
+  when "delete"
+    puts "Deleted!"
+  else
+    puts "Error!"
+end
+=end
+
+puts "### Prompting: Redux! ###"
+case choice
+  when "add"
+    puts "What movie would you like to add? "
+    title = gets.chomp
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_s] = rating
   when "update"
     puts "Updated!"
   when "display"
