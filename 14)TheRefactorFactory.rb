f151f3d3cd8bc99699ba02369b6e99fb833456ef
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-25 18:00:40 +0300

    Omit Needless Words

--- 14)TheRefactorFactory.rb
+++ 14)TheRefactorFactory.rb
@@ -2,3 +2,35 @@
  Refactoring is the process by which we improve a code's structure,
  appearance, and/or performance without modifying its overall behavior.
 =end
+
+puts "### Omit Needless Words ###"
+=begin
+  require 'prime'   # This is a module.
+
+  def first_n_primes(n)
+
+    unless n.is_a? Integer
+      return "n must be an integer."
+    end
+
+    if n <= 0
+      return "n must be greater than 0."
+    end
+
+    return Prime.first n
+  end
+
+first_n_primes(10)
+=end
+require 'prime'   # This is a module.
+
+def first_n_primes(n)
+
+  return "n must be an integer." unless n.is_a? Integer
+
+  return "n must be greater than 0." if n <= 0
+
+  return Prime.first n
+end
+
+puts first_n_primes(10)
