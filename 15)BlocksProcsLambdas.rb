5dfcf366e481da3624a3c3e46d18f99772b80bce
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-27 00:02:01 +0300

    Yielding With Parameters

--- 15)BlocksProcsLambdas.rb
+++ 15)BlocksProcsLambdas.rb
@@ -19,3 +19,16 @@ def block_test
 end
 
 block_test { puts ">>> We're in the block!" }
+
+puts "### Yielding With Parameters ###"
+def yield_name(name)
+  puts "In the method! Let's yield."
+  yield("Kim")
+  puts "In between the yields!"
+  yield(name)
+  puts "Block complete! Back in the method."
+end
+
+yield_name("Eric") { |n| puts "My name is #{n}." }
+
+yield_name("Alex") { |n| puts "My name is #{n}." }
