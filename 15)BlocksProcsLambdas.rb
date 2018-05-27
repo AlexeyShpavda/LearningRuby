ad9f4f2fcedab90105c9034964c4c26befab58cd
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-27 21:35:13 +0300

    Implementation of Lambdas vs. Procs

--- 15)BlocksProcsLambdas.rb
+++ 15)BlocksProcsLambdas.rb
@@ -104,3 +104,18 @@ symbols = strings.collect(&symbolize)
 print symbols, "\n"
 
 puts "### Lambdas vs. Procs ###"
+def batman_ironman_proc
+  victor = Proc.new { return "Batman will win!" }
+  victor.call
+  "Iron Man will win!"
+end
+
+puts batman_ironman_proc
+
+def batman_ironman_lambda
+  victor = lambda { return "Batman will win!" }
+  victor.call
+  "Iron Man will win!"
+end
+
+puts batman_ironman_lambda
