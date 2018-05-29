4d7625da24ae52de6d299fa0d19712e8db029274
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-29 19:13:21 +0300

    Fancify Initialize Method

--- 17)VirtualComputer.rb
+++ 17)VirtualComputer.rb
@@ -1,5 +1,16 @@
 puts "### Create Class ###"
+=begin
 class Computer
   def initialize
+  end
+end
+=end
+
+puts "### Fancify Initialize Method ###"
+class Computer
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = Hash.new()
   end
 end
