94cdab440b319bae7d62c7a482eb8d0ccc58da24
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-29 19:16:24 +0300

    Have a Little Class

--- 17)VirtualComputer.rb
+++ 17)VirtualComputer.rb
@@ -7,10 +7,23 @@ end
 =end
 
 puts "### Fancify Initialize Method ###"
+=begin
 class Computer
   def initialize(username, password)
     @username = username
     @password = password
     @files = Hash.new()
+  end
+end
+=end
+
+puts "### Have a Little Class ###"
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
   end
 end
