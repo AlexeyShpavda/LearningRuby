be5b28893befc545ec22a4f12e3661fa14302e77
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-29 18:16:19 +0300

    Twice the @, Twice as Classy

--- 16)ObjectOrientedProgramming1.rb
+++ 16)ObjectOrientedProgramming1.rb
@@ -38,3 +38,22 @@ class Human
     @profession = profession
   end
 end
+
+puts "### Twice the @, Twice as Classy ###"
+class Person
+  @@people_count = 0
+
+  def initialize(name)
+    @name = name
+    @@people_count += 1
+  end
+
+  def self.number_of_instances
+    @@people_count
+  end
+end
+
+matz = Person.new("Yukihiro")
+dhh = Person.new("David")
+
+puts "Number of Person instances: #{Person.number_of_instances}"
