bddd2500c25b2f60b29acd115d8b87931379ad1d
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-30 22:58:53 +0300

    Going Public

--- 18)ObjectOrientedProgramming2.rb
+++ 18)ObjectOrientedProgramming2.rb
@@ -4,3 +4,17 @@ class ClassName
     @class_Variable = parameter
   end
 end
+
+puts "### Going Public ###"
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  public
+
+  def bark
+    puts "Woof!"
+  end
+end
