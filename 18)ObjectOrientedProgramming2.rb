63ddd165b46765dadfb078a69cfbd18f78c7db2e
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-30 23:14:24 +0300

    attr_accessor

--- 18)ObjectOrientedProgramming2.rb
+++ 18)ObjectOrientedProgramming2.rb
@@ -18,3 +18,45 @@ class Dog
     puts "Woof!"
   end
 end
+
+puts "### Private! Keep Out! ###"
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  public
+
+  def bark
+    puts "Woof!"
+  end
+
+  private
+
+  def id
+    @id_number = 12345
+  end
+end
+
+puts "### attr_reader, attr_writer ###"
+class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  attr_reader :name
+  attr_writer :job
+end
+
+puts "### attr_accessor ###"
+class Person
+  attr_reader :name
+  attr_accessor :job
+
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+end
