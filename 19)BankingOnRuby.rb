8d226da5603688003416bb45c8d4fdb829deae0a
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-31 17:11:23 +0300

    Opening an Account

--- 19)BankingOnRuby.rb
+++ 19)BankingOnRuby.rb
@@ -58,6 +58,7 @@ end
 =end
 
 puts "### Making a Withdrawal ###"
+=begin
 class Account
   attr_reader :name, :balance
   def initialize(name, balance=100)
@@ -90,3 +91,40 @@ class Account
     "Access denied: incorrect PIN."
   end
 end
+=end
+
+puts "### Opening an Account ###"
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  public
+
+  def display_balance(pin_number)
+    puts pin_number == @pin ? "Balance: $#{@balance}." : "pin_error"
+  end
+
+  def withdraw(pin_number, amount)
+    if pin_number == @pin
+      @balance -= amount
+      puts "Withdrew #{amount}."
+    else
+      puts pin_error
+    end
+  end
+
+  private
+
+  def pin
+    @pin = 1234
+  end
+
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+end
+
+checking_account = Account.new("Alex", 1000000)
