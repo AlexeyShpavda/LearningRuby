a2a0e98edc8032d3a2ecf6977b0bd25a484c41da
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-31 16:58:02 +0300

    Displaying the Balance

--- 19)BankingOnRuby.rb
+++ 19)BankingOnRuby.rb
@@ -10,6 +10,7 @@ end
 =end
 
 puts "### Private Affairs ###"
+=begin
 class Account
   attr_reader :name, :balance
   def initialize(name, balance=100)
@@ -21,6 +22,30 @@ class Account
 
   def pin(number=1234)
     @pin = number
+  end
+
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+end
+=end
+
+puts "### Displaying the Balance ###"
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  def display_balance(pin_number)
+    puts @pin == pin_number ? "Balance: $#{@balance}." : "pin_error"
+  end
+
+  private
+
+  def pin
+    @pin = 1234
   end
 
   def pin_error
