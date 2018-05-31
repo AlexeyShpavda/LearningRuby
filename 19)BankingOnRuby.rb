1d60c232fdd83428faa5c48d090c9cef3fcbcc17
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-05-31 16:50:07 +0300

    Private Affairs

--- 19)BankingOnRuby.rb
+++ 19)BankingOnRuby.rb
@@ -1,8 +1,29 @@
 puts "### Creating the Account Class ###"
+=begin
 class Account
   attr_reader :name, :balance
   def initialize(name, balance=100)
     @name = name
     @balance = balance
+  end
+end
+=end
+
+puts "### Private Affairs ###"
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+
+  private
+
+  def pin(number=1234)
+    @pin = number
+  end
+
+  def pin_error
+    "Access denied: incorrect PIN."
   end
 end
